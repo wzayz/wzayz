2bb63bb6713e052c2cb6ea2764819937e57699dc
wzayz <40779120+wzayz@users.noreply.github.com>
2020-09-28 23:48:44 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+# 数据类型--容器1：列表
+list_v1 = [23,24,25,26]    # 列表
+tuple_v1 = ('曹操','刘备','李世民','武则天')   # 元组
+dic_v1 = {"年龄":list_v1,"姓名":tuple_v1}    # 字典
+set_v1 = {2,2,3,3,4,4,5,5,6,6}  # 集合(集合中的值，不重复)
+
+print(list_v1,type(list_v1))
+print(tuple_v1,type(tuple_v1))
+print(dic_v1,type(dic_v1))
+print(set_v1,type(set_v1))
